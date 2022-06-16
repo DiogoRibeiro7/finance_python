794c6c6d3b37d73b6b05bcaaabf99e150fb9bb5e
DiogoRibeiro7 <diogo_dj@hotmail.com>
2022-06-16 15:43:14 +0100

    chore: second commit

--- cobb_douglas.py
+++ cobb_douglas.py
@@ -1,5 +1,5 @@
 def cobb_douglas(K, L):
-    
+
     # Create alpha and z
     z = 1
     alpha = 0.33
@@ -7,4 +7,26 @@ def cobb_douglas(K, L):
     return z * K**alpha * L**(1 - alpha)
 
 
-print(cobb_douglas(1.0, 0.5) )
+print(cobb_douglas(1.0, 0.5))
+
+
+def returns_to_scale(K, L, gamma):
+    y1 = cobb_douglas(K, L)
+    y2 = cobb_douglas(gamma*K, gamma*L)
+    y_ratio = y2 / y1
+    return y_ratio / gamma
+
+
+print(returns_to_scale(1.0, 0.5, 2.0))
+
+
+def marginal_products(K, L, epsilon):
+
+    mpl = (cobb_douglas(K, L + epsilon) - cobb_douglas(K, L)) / epsilon
+    mpk = (cobb_douglas(K + epsilon, L) - cobb_douglas(K, L)) / epsilon
+
+    return mpl, mpk
+
+
+mpl, mpk = marginal_products(1.0, 0.5,  1e-4)
+print(f"mpl = {mpl}, mpk = {mpk}")
